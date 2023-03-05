9be7e317297559128105b8826d6cd06740e1ba6c
Casperbkkr <casperbkkr@gmail.com>
2023-03-05 19:33:00 +0100

    initial commit

---
+++ Stoch.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Stoch:
+	def __init__(self, dt, mu, sigma, n_steps, *, years=1, n_paths=1, seed=None, S0=100):
+		self.dt = dt
+		self.mu = mu
+		self.sigma = sigma
+		self.n_steps = n_steps
+		self.n_paths = n_paths
+		self.seed = seed
+		self.S0 = S0
+		self.years = years
+		self.Random_walk_paths = self._Random_walk(dt, n_steps, n_paths=n_paths, seed=seed)
+		self.GBM_paths = self._GBM(dt, mu, sigma, n_paths=n_paths, seed=seed, S0=S0)
+
+	def _Random_walk(self, dt, n_steps, *, n_paths=1, seed=None):
+		if seed is not None:
+			np.random.seed(seed)
+		return np.random.normal(0.0, np.sqrt(dt), [n_paths, n_steps])
+
+	def _GBM(self, dt, mu, sigma, *, n_paths=1, seed=None, S0=100):
+		St = (mu - sigma ** 2 / 2) * dt + sigma * self.Random_walk_paths
+		expSt = (np.exp(St)).T
+		expStplus = np.vstack([np.ones(n_paths), expSt])
+		gbm_paths = S0 * expStplus.cumprod(axis=0)
+		return gbm_paths
+
+	def show_paths(self):
+
+		# Define time interval correctly
+		time = np.linspace(0, self.years, self.n_steps+1)
+		# Require numpy array that is the same shape as St
+		tt = np.full(shape=(self.n_paths, self.n_steps+1), fill_value=time).T
+
+
+		plt.plot(tt, self.GBM_paths)
+		plt.xlabel("Years $(t)$")
+		plt.ylabel("Stock Price $(S_t)$")
+		plt.title(
+		"Realizations of Geometric Brownian Motion\n $dS_t = \mu S_t dt + \sigma S_t dW_t$\n $S_0 = {0}, \mu = {1}, \sigma = {2}$".format(self.S0, self.mu, self.sigma)
+		)
+		plt.show()
+
+
---
+++ brownian motion.py
@@ -0,0 +1,31 @@
+from Stoch import Stoch
+
+
+# Parameters
+# drift coefficent
+mu = 0.1
+# number of steps
+n_steps = 100
+# time in years
+years = 1
+# number of sims
+n_paths = 100
+# initial stock price
+S0 = 100
+# volatility
+sigma = 0.3
+dt=years/n_steps
+
+W1 = Stoch(dt, mu, sigma, n_steps, n_paths=1000)
+
+
+
+
+def dWs(upper, dt, *, path=None):
+	if path is None:
+		path = Wiener(upper/dt, dt)
+	else:
+		pass
+	return sum((upper - np.arange(dt, upper+dt, dt)) * np.diff(path))
+
+
