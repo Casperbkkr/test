7189088aaec6ea037edcbfd5a94389b7edd0f269
Casperbkkr <casperbkkr@gmail.com>
2023-03-06 14:54:15 +0100

    Modeled RV X(t)

--- 1.4.py
+++ 1.4.py
@@ -1,10 +1,25 @@
 from Stoch import Stoch
 import numpy as np
+import matplotlib.pyplot as plt
 
+# define dt
 dt = 0.001
+# define upper bound T
 T = 10
-n_steps = int(10/dt)
+# calculate number of timesteps
+n_steps = int(T/dt)
+# model Wiener process
 Wt = Stoch(dt, 1, 1, n_steps).Wiener_paths
 
-t_div_T = (np.arange(0, T, dt))/T
-Xt = Wt
+# create time array
+time = np.arange(0, T+dt, dt)
+# calculate t/T for all t
+t_div_T = time/T
+# reverse the Wiener process to obtain W(10-t)
+WT_min_t = np.flip(Wt, 1)
+# calculate X(t)
+Xt = Wt - (t_div_T * WT_min_t)
+
+# plot results
+plt.plot(time, Xt[0])
+plt.show()
