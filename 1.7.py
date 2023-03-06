e8abd9918b1d67bdbc81cfe1f7144c28cac81d97
Casperbkkr <casperbkkr@gmail.com>
2023-03-06 15:27:08 +0100

    How is RSSI defined on daily stock data?

--- 1.7.py
+++ 1.7.py
@@ -6,6 +6,10 @@ def calc_daily_returns(df_data):
 	new_col = df_data['Close'] - df_data['Open']
 	return new_col
 
+def calc_RSSI(df_data):
+	df_data
+	return
+
 sheet = pd.read_csv('daily_returns.csv')
 
 folder="/Users/casperbakker/Documents/PycharmProjects/Modelling_biosystems/Computational_Finance/stock_market_data/nasdaq/csv"
@@ -13,8 +17,6 @@ folder="/Users/casperbakker/Documents/PycharmProjects/Modelling_biosystems/Compu
 df_output = pd.DataFrame()  # make empty output Dataframe
 i=0
 for file in Path(folder).glob('*.csv'): # loop through all files in data
-    break
-	i += 1
 	stock_csv = str(file).split("/")[-1]  # get last part of file path
 	stock_name = stock_csv.split(".")[0]  # get stock name without .csv extension
 	print(i)
@@ -23,7 +25,7 @@ for file in Path(folder).glob('*.csv'): # loop through all files in data
 	df_daily_returns = pd.DataFrame()
 	df_daily_returns[stock_name] = calc_daily_returns(df_stock)
 	df_output =  pd.concat([df_output, df_daily_returns], axis=1)
-    if i == 1000
+	if i == 1000:
 		df_output.to_csv("daily_returns.csv")
 		break
 
