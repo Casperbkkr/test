a75c28955e101b81349644207a1bc7c051b9ac3c
Casperbkkr <casperbkkr@gmail.com>
2023-03-06 13:59:40 +0100

    Finished exercise 1.2. Does not seem accurate enough?

--- 1.2.py
+++ 1.2.py
@@ -1,24 +1,36 @@
 from Stoch import Stoch
 import numpy as np
 # Parameters
-# drift coefficent
+# Drift coefficent
 mu = 0.1
-# volatility
+# Volatility
 sigma = 0.3
-dt = 0.001
+# dt
+dt = 0.000001
 
 n_steps = int(5/dt)
+if n_steps % 5 != 0:  # make sure that steps are correct amount
+	n_steps = n_steps+1
 
 # Create paths of wiener process
-paths = Stoch(dt, mu, sigma, n_steps, n_paths=1)
+paths = Stoch(dt, mu, sigma, n_steps, n_paths=10)
 Wiener_path = paths.Wiener_paths
+
+# First calculate the right part of the equation
 
 # Calculate increments of Wiener process
 dWs = np.diff(Wiener_path)
 
 # Multiply by the 5-tdt
-int_dWs = (5 - np.arange(dt, 5, dt)) * dWs
+int_dWs = (5 - np.arange(dt, 5+dt, dt)) * dWs
 
 # Sum to finish integrating
 S_int_dWs = int_dWs.sum(axis=1)
 
+# Now calculate the left part of the equation
+int_ds = np.trapz(Wiener_path, dx=dt)
+
+
+# Calculate error between integrals of the same path
+error = S_int_dWs - int_ds
+print(error)
