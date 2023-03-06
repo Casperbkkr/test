b0a24ebbb327422590fb11db6736b64e26730c9f
Casperbkkr <casperbkkr@gmail.com>
2023-03-06 14:39:47 +0100

    started 1.4

--- 1.4.py
+++ 1.4.py
@@ -2,6 +2,9 @@ from Stoch import Stoch
 import numpy as np
 
 dt = 0.001
-Wt = Stoch(dt, 1, 1, 1).Wiener_paths
+T = 10
+n_steps = int(10/dt)
+Wt = Stoch(dt, 1, 1, n_steps).Wiener_paths
 
-Xt =1
+t_div_T = (np.arange(0, T, dt))/T
+Xt = Wt
