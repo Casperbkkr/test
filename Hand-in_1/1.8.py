36e1d888a735fa3d7df143fbda45cf4926e8a609
Casperbkkr <casperbkkr@gmail.com>
2023-03-20 14:28:26 +0100

    made plot of stocks from exercise 7 in exercise 8. Still need to add the right x-axis to the plot at 8c)

--- Hand-in_1/1.8.py
+++ Hand-in_1/1.8.py
@@ -1,12 +1,12 @@
 from Stoch import Stoch
+from Correlation import Correlation
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
 
 
-def calc_RSSI(data) -> pd.DataFrame:
-	df_data = data
+def calc_RSSI(df_data) -> pd.DataFrame:
 	df_out = pd.DataFrame()
 	for column in df_data.columns:
 		dSt = np.diff(df_data[column].values)
@@ -44,23 +44,37 @@ stock_paths = GBM.GBM_paths
 # show stock paths
 GBM.show_paths()
 
-
-
 # b)
 # calculate the running sum of square increments
 RSSI = GBM.RSSI
 
-#plot the RSSI
+# plot the RSSI
 plt.plot(RSSI)
 plt.show()
 
-
-
 # c)
+# get the daily returns of maximally and minimally correlated stocks from exercise 7.
+C1 = Correlation("daily_returns.csv")
+
+# get stock names
+n1, n2 = C1.min_stock_names
+n3, n4 = C1.max_stock_names
+
+# make new dataframew with the max-min corr stocks combined.
+s1 = C1.min_s1.to_numpy()
+s2 = C1.min_s2.to_numpy()
+s3 = C1.max_s1.to_numpy()
+s4 = C1.max_s2.to_numpy()
+m = (np.vstack([s1, s2, s3, s4])).T
+df = pd.DataFrame(data=m, columns=[n1, n2, n3, n4])
+
 # Calculate the RSSI for stocks
-df = pd.read_csv("daily_returns.csv", index_col="Date")
 df_RSSI = calc_RSSI(df)
 
+# TODO add correct x-axis with time
 # plot the stock data RSSI
+plt.plot(df_RSSI.iloc[:, 0].values)
+plt.plot(df_RSSI.iloc[:, 1].values)
 plt.plot(df_RSSI.iloc[:, 2].values)
+plt.plot(df_RSSI.iloc[:, 3].values)
 plt.show()
